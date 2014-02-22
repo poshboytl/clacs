c7322470b1daee09754c6896d82a0a10126e4303
Terry Tai <poshboytl@gmail.com>
2014-02-22 21:27:00 +0800

    Added the messages action to handle the users's message.

--- klacs.rb
+++ klacs.rb
@@ -2,4 +2,14 @@ class Klacs < Sinatra::Base
   get '/' do
     200
   end
+
+  post '/messages' do
+    Klacs.deliver(params[:message]) if params[:message]
+    201
+  end
+
+  def self.deliver(message)
+    # send to pusher
+  end
+
 end
--- spec/klacs_spec.rb
+++ spec/klacs_spec.rb
@@ -16,4 +16,25 @@ describe Klacs do
       last_response.status.should == 200
     end
   end
+
+  describe "POST /messages" do
+    let(:message) { 'hola!' }
+
+
+    context "with message params" do
+      it "should be success and call deliver" do
+        Klacs.should_receive(:deliver).with(message)
+        post '/messages', message: message
+        last_response.status.should == 201
+      end
+    end
+
+    context "without message params" do
+      it "should be success but without calling deliver" do
+        Klacs.should_not_receive(:deliver)
+        post '/messages'
+        last_response.status.should == 201
+      end
+    end
+  end
 end
